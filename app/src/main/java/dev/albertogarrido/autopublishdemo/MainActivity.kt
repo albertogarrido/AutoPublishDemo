56a5e35ab03ddc58a94fc8e7498d14f9931c9d6e
Alberto Garrido <hello@albertogarrido.net>
2020-08-28 11:30:20 +0200

    initial commit

---
+++ app/src/main/java/dev/albertogarrido/autopublishdemo/MainActivity.kt
@@ -0,0 +1,13 @@
+package dev.albertogarrido.autopublishdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        greeting.text = "Welcome to ${getString(R.string.app_name)}"
+    }
+}
