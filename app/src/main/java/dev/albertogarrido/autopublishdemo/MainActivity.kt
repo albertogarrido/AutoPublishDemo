9085b1479c449502332255e62f7411f970c5201d
Alberto Garrido <hello@albertogarrido.net>
2020-09-04 11:33:46 +0200

    wil

--- app/src/main/java/dev/albertogarrido/autopublishdemo/MainActivity.kt
+++ app/src/main/java/dev/albertogarrido/autopublishdemo/MainActivity.kt
@@ -7,7 +7,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)\
+
         greeting.text = "Welcome to ${getString(R.string.app_name)}"
     }
 }
