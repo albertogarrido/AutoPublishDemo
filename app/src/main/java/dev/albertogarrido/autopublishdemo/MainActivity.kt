eb534971c4058034b2731536f381c7a3e0c740fb
Alberto Garrido <hello@albertogarrido.net>
2020-09-04 11:05:09 +0200

    changes in main activity

--- app/src/main/java/dev/albertogarrido/autopublishdemo/MainActivity.kt
+++ app/src/main/java/dev/albertogarrido/autopublishdemo/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         greeting.text = "Welcome to ${getString(R.string.app_name)}"
+
     }
 }
